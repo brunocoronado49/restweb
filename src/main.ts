7c96f456ef526dfbd48d4a63c78ab72114a2576e
brunocoronado49 <francorangelcoronado@gmail.com>
2026-02-04 12:21:30 -0600

    Add http2 for examples in webserver

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,49 @@
-function main() {
-  const message: string = "Hello World!";
-  console.log(message);
-}
+import fs from "fs";
+import http2 from "http2";
 
-main();
+const server = http2.createSecureServer(
+  {
+    key: "",
+    cert: "",
+  },
+  (req, res) => {
+    console.log(req.url);
+
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    // res.write("<h1>Hello World!</h1>");
+    // res.end();
+
+    // const data = { name: "Jhon Wick", age: 40, city: "New York" };
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    // res.end(JSON.stringify(data));
+
+    // if (req.url === "/") {
+    //   const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.end(htmlFile);
+    // } else {
+    //   res.writeHead(404, { "Content-Type": "text/html" });
+    //   res.end();
+    // }
+
+    if (req.url === "/") {
+      const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(htmlFile);
+      return;
+    }
+
+    if (req.url?.endsWith(".js")) {
+      res.writeHead(200, { "Content-Type": "application/javscript" });
+    } else if (req.url?.endsWith(".css")) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+    }
+
+    const responseContent = fs.readFileSync(`./public${req.url}`, "utf-8");
+    res.end(responseContent);
+  },
+);
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
