f0329f2286f8bf8a834062ba0e05460d0e6a62f2
brunocoronado49 <francorangelcoronado@gmail.com>
2026-02-23 10:08:55 -0600

    Add DTO and change the controllers

--- src/presentation/todos/constroller.ts
+++ src/presentation/todos/constroller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos/dtos";
 
 export class TodosController {
     constructor() {}
@@ -24,11 +25,12 @@ export class TodosController {
 
     //* create new Todo task
     public createTodo = async (req: Request, res: Response) => {
-        const { text } = req.body;
-        if (!text) return res.status(400).json({ error: "Text property is required." });
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+
+        if (error) return res.status(400).json({ error });
 
         const newTodo = await prisma.todo.create({
-            data: { text },
+            data: createTodoDto!,
         });
 
         res.json(newTodo);
@@ -37,7 +39,12 @@ export class TodosController {
     //* update one Todo task
     public updateTodo = async (req: Request, res: Response) => {
         const id: number = Number(req.params.id);
-        if (isNaN(id)) return res.status(400).json({ error: "Id argument is not a number." });
+        const [error, updateTodoDto] = UpdateTodoDto.create({
+            ...req.body,
+            id,
+        });
+
+        if (error) return res.status(400).json({ error });
 
         const todo = await prisma.todo.findFirst({
             where: { id },
@@ -45,14 +52,9 @@ export class TodosController {
 
         if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
 
-        const { text, completedAt } = req.body;
-
         const updatedTodo = await prisma.todo.update({
             where: { id },
-            data: {
-                text,
-                completedAt: completedAt ? new Date(completedAt) : null,
-            },
+            data: updateTodoDto!.values,
         });
 
         res.json(updatedTodo);
